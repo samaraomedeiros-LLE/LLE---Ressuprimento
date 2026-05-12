e1b35bfb19aef0ad98e348c5c4dfd80356e875a7
samaraomedeiros-LLE <samara.o.medeiros@gmail.com>
2026-05-12 18:02:59 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
 import numpy as np
+import io
 
 # ─── CONFIG ───────────────────────────────────────────────────────────────────
 st.set_page_config(
